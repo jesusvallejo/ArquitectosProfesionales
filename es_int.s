d14d7ee7a8de568d8d2c172a8ac0c50f7b02f2b8
jesus.vallejo.collados@gmail.com <jesus.vallejo.collados@gmail.com>
2020-02-18 20:28:06 +0100

    update

--- es_int.s
+++ es_int.s
@@ -732,7 +732,8 @@ INICIO:
 		MOVE.W			#13,D1
 		BSR				ESCCAR
 		MOVE.L			#0,A0 
-        MOVE.L			#BUFP,A0  
+        MOVE.L			#BUFP,A0
+			ADD.LA		#6,A0
             MOVE.W 		#0,D0
             MOVE.W 		#8,D3
             MOVE.W 		D3,-(A7)
